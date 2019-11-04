bff84b88299eac420b8bddde846483bc669f7217
Diego N. Venturi <diegoventuri@gmail.com>
2019-11-04 11:58:34 -0300

    initial commit

---
+++ 2lKEps_lambda/2lKEps_lambda.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('default')
+
+A_2layer = 0.05*200*2/np.log((1.+0.98)/(1.-0.98))
+Rey = np.linspace(150,250,1000)
+lambdaa = 0.5*(1+np.tanh((Rey-200)/A_2layer))
+
+plt.style.use('oneHalfColumn.mplstyle')
+fig, ax = plt.subplots()
+ax.set_xlabel(r'$\lambda$')
+ax.set_ylabel(r'$\mathrm{Re}_y$')
+ax.axis([185, 215.0, 0, 1])
+ax.xaxis.set_major_locator(plt.MultipleLocator(10))
+ax.xaxis.set_minor_locator(plt.MultipleLocator(5))
+ax.yaxis.set_major_locator(plt.MultipleLocator(0.2))
+ax.yaxis.set_minor_locator(plt.MultipleLocator(0.1))
+ax.plot(Rey, lambdaa,
+        color='black',
+        linewidth=1,
+        linestyle='-')
+fig.tight_layout(pad=0.01)
+plt.savefig('2lKEps_lambda.pdf',
+            format='pdf')
